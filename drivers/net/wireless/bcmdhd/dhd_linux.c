0bb6b2af722badead8b25f7ea2039eace7d59f8e
sonicxml <sonicxml@gmail.com>
2012-02-27 17:19:29 -0500

    imoseyon - reduce wakelocks by lte and wifi

--- drivers/net/wireless/bcmdhd/dhd_linux.c
+++ drivers/net/wireless/bcmdhd/dhd_linux.c
@@ -4685,6 +4685,7 @@ int dhd_os_wake_lock_timeout(dhd_pub_t *pub)
 		if (dhd->wakelock_ctrl_timeout_enable)
 			wake_lock_timeout(&dhd->wl_ctrlwake,
 				msecs_to_jiffies(dhd->wakelock_ctrl_timeout_enable));
+
 #endif
 		dhd->wakelock_rx_timeout_enable = 0;
 		dhd->wakelock_ctrl_timeout_enable = 0;
